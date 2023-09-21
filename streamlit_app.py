bde553a442f353ffb2f9fc04c3569906f6f2bba5
hama-syamu <144981151+hama-syamu@users.noreply.github.com>
2023-09-21 04:31:59 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,8 @@ import streamlit as st
 import random
 st.title('おみくじアプリ')
 
-if st.button('おみくじを引く')
-   reaults = ['大吉','中吉','小吉','吉','凶','大凶']
-   
-   result = random.choice(results)
+if st.button('おみくじを引く'):
 
-   st.write(f'結果:{result}')
+    reaults = ['大吉','中吉','小吉','吉','凶','大凶']
+    result = random.choice(results)
+    st.write(f'結果:{result}')
